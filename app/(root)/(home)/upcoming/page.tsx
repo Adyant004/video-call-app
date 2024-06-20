1536ee081714eae76de923d84902a19fae11fe96
Adyant <adyantajay@gmail.com>
2024-06-20 15:52:04 +0530

    Initial Commit

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,7 +1,7 @@
 import CallList from '@/components/CallList'
 import React from 'react'
 
-const Upcoming = ({ type } : { type: 'upcoming' | 'ended' | 'recordings' }) => {
+const Upcoming = () => {
   return (
     <section className='flex size-full flex-col gap-10'>
       <h1 className='text-3xl font-bold'>
